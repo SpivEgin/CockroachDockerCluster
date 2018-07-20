0de11847081a35275b21a78fe163f203f8863ef0
SpivEgin <spivegin@txtsme.com>
2018-07-20 03:53:35 -0400

    bin changed

--- docker/bash/user_management_entry.sh
+++ docker/bash/user_management_entry.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/dumb-init /bin/sh
 
-sleep 30
+sleep 45
 tlmuser init
 tlmuser adduser --username root
 cockroach init --certs-dir=/opt/tlmcockroach/cockroachCerts --host=172.18.0.3:26260
