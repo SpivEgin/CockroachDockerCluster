8d52217efd0aebec1baf136fa399b1f1e4913abd
SpivEgin <spivegin@txtsme.com>
2018-07-23 14:46:18 -0400

    Added Caddy custom build

---
+++ docker/bash/caddy_entry.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+
+if ${TESTING} == "true"; then
+    caddy  -agree=true-email=ssl@txtsme.com -conf=/opt/caddy/Caddyfile -quiet=true -ca=https://acme-staging-v02.api.letsencrypt.org/directory -log=/var/log/caddy/server.log
+    else
+    caddy  -agree=true-email=ssl@txtsme.com -conf=/opt/caddy/Caddyfile -quiet=true -log=/var/log/caddy/server.log
+fi
+
