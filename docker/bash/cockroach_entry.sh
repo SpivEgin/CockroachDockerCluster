f4095cd9a437cc4f14bf35086ae4fe7af8986367
SpivEgin <spivegin@txtsme.com>
2018-07-24 01:24:17 -0400

    trying updated timming

--- docker/bash/cockroach_entry.sh
+++ docker/bash/cockroach_entry.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/dumb-init /bin/sh
 
-sleep 2
+sleep 6
 tlmkeyc init --node
 tlmkeyc cockroach
--- docker/bash/keysign_entry.sh
+++ docker/bash/keysign_entry.sh
@@ -2,7 +2,7 @@
 
 
 tlmkeys init --keysign
-# sleep 10
+sleep 3
 tlmkeys nodecluster &
 tlmkeys users &
 tlmkeys keysign
