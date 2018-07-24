4838f56ec77c8d3c8d1b1854b14cc0fda5651aca
SpivEgin <spivegin@txtsme.com>
2018-07-23 20:38:35 -0400

    fixed typo in bash

--- docker/bash/caddy_entry.sh
+++ docker/bash/caddy_entry.sh
@@ -2,8 +2,8 @@
 
 
 if ${TESTING} == "true"; then
-    caddy  -agree=true-email=ssl@txtsme.com -conf=/opt/caddy/Caddyfile -quiet=true -ca=https://acme-staging-v02.api.letsencrypt.org/directory -log=/var/log/caddy/server.log
+    caddy  -agree=true -email=ssl@txtsme.com -conf=/opt/caddy/Caddyfile -quiet=true -ca=https://acme-staging-v02.api.letsencrypt.org/directory -log=/var/log/caddy/server.log
     else
-    caddy  -agree=true-email=ssl@txtsme.com -conf=/opt/caddy/Caddyfile -quiet=true -log=/var/log/caddy/server.log
+    caddy  -agree=true -email=ssl@txtsme.com -conf=/opt/caddy/Caddyfile -quiet=true -log=/var/log/caddy/server.log
 fi
 
