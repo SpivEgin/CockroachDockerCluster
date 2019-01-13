1d38134f46fa7d3f6810bf0c89f6db3943194c8c
SpivEgin <spivegin@txtsme.com>
2019-01-12 23:38:43 -0500

    removed init for user entry caus of tlmuser norlonger producing init.sh

--- docker/bash/user_management_entry.sh
+++ docker/bash/user_management_entry.sh
@@ -3,7 +3,7 @@
 sleep 100
 tlmuser init
 tlmuser rootuser
-tlmuser startcluster --IsCluster $NODE_CLUSTER
 echo "Initialing Cluster..."
-chmod +x init.sh && ./init.sh
+tlmuser startcluster --IsCluster $NODE_CLUSTER
+# chmod +x init.sh && ./init.sh
 sleep 120
